3e3cf2ff88b0c162ee5c7f7437e427862cb39056
Branislav Vidojevic <branislav.vidojevic@fioneer.com>
2023-11-01 10:28:33 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Button from 'remoteApp/Button'
+import Widget from 'remoteApp/Widget'
+
+function App() {
+  return (
+    <div className="card">
+      <Button />
+      <Widget />
+    </div>
+  )
+}
+
+export default App
